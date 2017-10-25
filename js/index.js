13826a58de9ef437e4fde083bdc6944cad87297f
itmandqw <1046715064@qq.com>
2017-10-25 19:13:56 +0800

    完整版QQTIM视差滚动

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zhousg on 2017/10/9.
+ */
+//$(function(){
+//    $.stellar({
+//        horizontalScrolling:false,
+//        responsive:true
+//    });
+//})
+$(document).ready(function(){
+    $.stellar({
+        horizontalScrolling:false,
+        responsive:true
+    });
+})
+
+//window.onload=function(){
+//    //alert('666');
+//}
